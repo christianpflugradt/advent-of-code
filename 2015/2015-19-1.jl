2577f6a3b0b785ba54e03a6257abd21406134fc8
Christian Pflugradt <dev@pflugradts.de>
2024-04-01 12:43:32 +0200

    solved day 19 of 2015

---
+++ 2015/2015-19-1.jl
@@ -0,0 +1,66 @@
+input = raw"Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg"
+
+sequence = "CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl"
+
+function solve(input)
+	mutations = Set{String}()
+    regex = r"([A-Za-z]+) => ([A-Za-z]+)"
+	for line in split(input, "\n")
+		m = match(regex, line)
+		start = 1
+		nrange = findnext(m[1], sequence, start)
+		while nrange != nothing
+			push!(mutations, (nrange[1] == 1 ? "" : sequence[1:nrange[1]-1]) *
+				m[2] * sequence[nrange[1] + length(nrange):length(sequence)])
+			start = nrange[1] + 1
+			nrange = findnext(m[1], sequence, start)
+		end
+	end
+	return length(mutations)
+end
+
+output = solve(input)
+
+println(output)
---
+++ 2015/2015-19-2.jl
@@ -0,0 +1,71 @@
+input = raw"Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg"
+
+sequence = "CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl"
+
+tlen(t) = length(t[2])
+
+function solve(input, sequence)
+    regex = r"([A-Za-z]+) => ([A-Za-z]+)"
+	rs = Vector{Tuple{String,String}}()
+	for line in split(input, "\n")
+		m = match(regex, line)
+		push!(rs, (m[1], m[2]))
+	end
+	sort!(rs, by=tlen, rev=true)
+	modifications = 0
+	while sequence != "e"
+		r = 1
+		while findfirst(rs[r][2], sequence) == nothing
+			r += 1
+		end
+		sequence = replace(sequence, rs[r][2] => rs[r][1], count = 1)
+		modifications += 1
+	end
+	return modifications
+end
+
+output = solve(input, sequence)
+
+println(output)
