8cfea8777bf1b4085de568bfe8831e3bbdbaf3ed
Christian Pflugradt <110765548+christianpflugradt@users.noreply.github.com>
2025-02-02 13:51:09 +0100

    improve performance of day 10 of 2015

--- 2015/2015-10-1.jl
+++ 2015/2015-10-1.jl
@@ -2,23 +2,25 @@ input = raw"3113322113"
 
 function solve(input)
 	next_code = input
+	buffer = IOBuffer()
 	for i in 1:40
 		prv = ""
 		repetition = 1
 		code = next_code
-		next_code = ""
+		truncate(buffer, 0)
 		for chr in code
 			if prv != ""
 				if chr == prv
 					repetition += 1
 				else
-					next_code = "$next_code$repetition$prv"
+					print(buffer, repetition, prv)
 					repetition = 1
 				end
 			end
 			prv = chr
 		end
-		next_code = "$next_code$repetition$prv"
+		print(buffer, repetition, prv)
+		next_code = String(take!(buffer))
 	end
 	return length(next_code)
 end
